bbf64f4d451e24807b58c8582ca5215be8f55837
arun9959-23 <23r11a6710@gcet.edu.in>
2025-05-21 20:46:53 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Router, Routes } from 'react-router-dom'
+import Product from 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div>
+      <Router>
       <h1>My  online Shop</h1>
+      <link to="/">Home</link>
+      <link to="/cart">Home</link>
       <hr />
       <header>
 
       </header>
+      
       <main>
+        <Routes>
+          <route index element={<product />}/>
+           <route path="/cart" element={<cart/>}/>
 <h2> products list</h2>
+</Routes>
       </main>
+
       <footer>
      <hr />
      &copy; 2005. All rights to users
 
       </footer>
+      </Router>
+      </div>
     </>
   )
 }
