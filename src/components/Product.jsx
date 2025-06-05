76df0842e0b03f4c3eead711eea32e8b0530bf50
arun9959-23 <23r11a6710@gcet.edu.in>
2025-06-05 12:23:58 +0530

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,35 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
-function Product() {
+import React, { useEffect, useState } from "react";
+import { useContext } from "react";
+import { AppContext } from "../App";
+import axios from "axios";
+import "./Product.css"
+export default function Product() {
+  const { user } = useContext(AppContext);
   const [products, setProducts] = useState([]);
-
+  const API = import.meta.env.VITE_API_URL;
+  const fetchProducts = async () => {
+    const res = await axios.get(`${API}/products/all`);
+    setProducts(res.data);
+  };
   useEffect(() => {
-    const API = import.meta.env.VITE_API_URL || "http://localhost:8080";
-
-    const fetchProducts = async () => {
-      try {
-        const res = await axios.get(`${API}/products`);
-        setProducts(res.data);
-      } catch (err) {
-        console.error("Failed to fetch products:", err);
-      }
-    };
-
     fetchProducts();
   }, []);
-
   return (
     <div>
-      <h2>Product Lists</h2>
-      <ul>
-        {products.map((prod, index) => (
-          <li key={index}>
-            {prod.name} - ${prod.price}</li>
-        ))}
-      </ul>
+      <h3>Welcome {user.name}! </h3>
+      <div className="App-Product-Row">
+        {products &&
+          products.map((value) => (
+            <div key={value._id}>
+              <h3>{value.name}</h3>
+              <h4>{value.price}</h4>
+              <button>Add to Cart</button>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
-
-export default Product;
